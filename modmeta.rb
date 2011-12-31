1e2ab6ebacd46a3c564d1be4c466cfc0ac926fae
markusn <markus@botten.org>
2011-12-31 02:45:47 +0100

    First working version. Can set title for .xm,.it and .mod files. Can display title for .xm,.it,.mod. Can display tracker for .xm

---
+++ modmeta.rb
@@ -0,0 +1,167 @@
+#!/usr/bin/ENV ruby
+# encoding: utf-8
+# -*- coding: utf-8 -*-
+################################################################################
+# Display and/or modify tracker mod data (.xm |.mod |.it)
+#
+# Author::    Markus Näsman  (mailto:markus [at] botten.org)
+# Copyright:: Copyright (c) 2011 Markus Näsman
+# License::   GPLv3
+################################################################################
+
+################################################################################
+# R E Q U I R E
+################################################################################
+require 'optparse'
+
+################################################################################
+# C O N S T A N T S
+################################################################################
+
+# File formats supported
+FILEFORMATS = ['.xm', '.mod', '.it']
+
+# Constants for .xm (http://content.gpwiki.org/index.php/XM)
+XM_TITLE_OFFSET_CONST   = 17
+XM_TITLE_LENGTH_CONST   = 20
+XM_TRACKER_OFFSET_CONST = 38
+XM_TRACKER_LENGTH_CONST = 20
+
+# Constants for .mod (http://www.aes.id.au/modformat.html)
+MOD_TITLE_OFFSET_CONST = 0
+MOD_TITLE_LENGTH_CONST = 20
+
+# Constant for .it (guessed...)
+IT_TITLE_OFFSET_CONST = 4
+IT_TITLE_LENGTH_CONST = 20
+
+################################################################################
+# V A R I A B L E S
+################################################################################
+options = {}
+
+################################################################################
+# F U N C T I O N S
+################################################################################
+def get_title_offset_len(filename)
+  case File.extname(filename)
+  when ".xm"
+    return XM_TITLE_OFFSET_CONST, XM_TITLE_LENGTH_CONST
+  when ".mod"
+    return MOD_TITLE_OFFSET_CONST, MOD_TITLE_LENGTH_CONST
+  when ".it"
+    return IT_TITLE_OFFSET_CONST, IT_TITLE_LENGTH_CONST
+  else
+    return -1, -1
+  end
+end
+
+def get_tracker_offset_len(filename)
+  case File.extname(filename)
+  when ".xm"
+    return XM_TRACKER_OFFSET_CONST, XM_TRACKER_LENGTH_CONST
+  else
+    return -1, -1
+  end
+end
+
+def display_metadata(filename)
+  display_title(filename)
+  display_tracker(filename)
+end
+
+def display_title(filename)
+  offset,len = get_title_offset_len(filename)
+  display_attr(filename, "title", offset, len)
+end
+
+def display_tracker(filename)
+  offset,len = get_tracker_offset_len(filename)
+  display_attr(filename, "tracker", offset, len)
+end
+
+def display_attr(filename, attr, offset, len)
+  v = "n/a"
+  if offset >=0 && len >= 0
+    f = open(filename)
+    f.pos = offset
+    v = f.read(len)
+    f.close()
+  end
+  puts "#{attr}: #{v}"
+end
+
+def set_title(filename, title)
+  offset,len = get_title_offset_len(filename)
+  if offset < 0 || len < 0
+    puts "Cannot set title for format"
+    exit(1)
+  end
+  if title.length > len
+    puts "title longer than maximum allowed length #{len}, aborting!"
+    exit(1)
+  else
+    # Create padded byte array from title
+    title_bytes = len.times.map { 0 }
+    i = 0
+    title.each_byte do |byte|
+      title_bytes[i] = byte
+      i +=1
+    end
+    # Read file
+    f = open(filename, 'r')
+    bytes = f.bytes.to_a
+    f.close()
+    # Make modifications
+    i = 0
+    title_bytes.each do |byte|
+      bytes[offset+i] = byte
+      i +=1
+    end
+    # Write file
+    f = open(filename, 'w')
+    bytes.each do | byte |
+      f.print byte.chr
+    end
+    f.close()
+  end
+end
+
+################################################################################
+# M A I N
+################################################################################
+if __FILE__ == $0
+  # Parse options
+  OptionParser.new do |opts|
+    opts.banner = "Usage: modmeta.rb [options] [filename]"
+    opts.on("-t", "--set-title [TITLE]", String, "Set title") do |title|
+      options[:title] = title
+    end
+    opts.on("-d", "--display", "Display metadata") do |v|
+      options[:display] = v
+    end
+    opts.on_tail("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  # Ensure correct number of files
+  if ARGV.length == 0
+    puts "Need to specify filename"
+    exit(1)
+  elsif ARGV.length == 1
+    filename = ARGV[0]
+    if not FILEFORMATS.include?(File.extname(filename))
+      puts "File format not supported"
+      exit(1)
+    end
+    if options[:display]
+      display_metadata(filename)
+    elsif options[:title]
+      set_title(filename, options[:title])
+    end
+  else
+    puts "Only one filename should be specified"
+  end
+end
